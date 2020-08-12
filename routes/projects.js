53dfbe5b2ebf14a0469af15dd3e1b6d6da59ee27
azis <abdlazis530@gmail.com>
2020-08-12 14:38:54 +0700

    display issue input data done

--- routes/projects.js
+++ routes/projects.js
@@ -52,7 +52,7 @@ module.exports = (db) => {
             }
 
         } else {
-            console.log(optionCheckBox)
+           
             let currentPage = req.query.page || 1
             let page = "page"
             let queryTotal = `SELECT COUNT(DISTINCT projects.projectid) FROM ((users JOIN members ON users.userid=members.userid)JOIN projects ON projects.projectid = members.projectid)`
@@ -322,9 +322,31 @@ module.exports = (db) => {
     // });
 
     // // localhost:3000/projects/issues/1
-    // router.get('/issues/:projectid', helpers.isLogIn, function (req, res, next) {
-    //     res.render('projects/issues/list')
-    // });
+    router.get('/issues/:projectid', helpers.isLogIn, async (req, res, next)=> {
+        // let currentPage = req.query.page || 1
+        // let page = "page"
+        // let queryTotal = `SELECT COUNT(DISTINCT projects.projectid) FROM ((users JOIN members ON users.userid=members.userid)JOIN projects ON projects.projectid = members.projectid)`
+        // let queryGetData = `SELECT projects.projectid, projects.name, STRING_AGG (users.firstname || ' ' || users.lastname,', 'ORDER BY users.firstname,users.lastname) members FROM((users JOIN members ON users.userid=members.userid)JOIN projects ON projects.projectid = members.projectid) GROUP BY projects.projectid LIMIT ${limit} OFFSET ${limit * currentPage - limit};`
+
+        // const total = await db.query(queryTotal)
+        // const fullname = await db.query("SELECT CONCAT(firstname, ' ', lastname) AS fullname FROM users")
+        // const getData = await db.query(queryGetData)
+
+        // let totalPage = Math.ceil(Number(total.rows[0].count) / limit)
+        // res.render('projects/view', { user: req.session.user, currentPage, totalPage, data: getData.rows, nameOfPage: page, fullnames: fullname.rows, optionCheckBox })
+        try {
+            const issuesQuery = `SELECT*FROM issues WHERE projectid=$1`
+            const issues = await db.query(issuesQuery,[req.params.projectid])
+            const getIssues = issues.rows
+    
+            console.log(getIssues)
+            const url =req.params.projectid
+            res.render('projects/issues/view',{url})
+        } catch (error) {
+            console.log(error)
+        }
+       
+    });
 
     // // localhost:3000/projects/issues/1/add
     // router.get('/issues/:projectid/add', helpers.isLogIn, function (req, res, next) {
